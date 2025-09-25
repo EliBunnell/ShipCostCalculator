b3a7090bae3f8665a56ff1772b3417e5824bace5
EliBunnell <bunnelec@mail.uc.edu>
2025-09-25 14:20:55 -0400

    Done

---
+++ src/main.java
@@ -0,0 +1,38 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        double itemPrice = 0.0;
+        final double SHIP_RATE = 0.02;
+        final double FREE_SHIP = 100.0;
+        double shipCost = 0.0;
+        double totalCost = 0.0;
+        String trash = "";
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the item price: ");
+        if(in.hasNextDouble()) {
+            itemPrice = in.nextDouble();
+            in.nextLine();
+
+            if (itemPrice >= FREE_SHIP) {
+
+                shipCost = 0;
+                totalCost = itemPrice;
+            }
+            else {
+                shipCost = itemPrice * SHIP_RATE;
+                totalCost = itemPrice + shipCost;
+            }
+
+            System.out.println("Total cost: " + totalCost);
+        }
+
+
+
+    }
+}
